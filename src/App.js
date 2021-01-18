bb42acb319ccceed013751de31cfd2c04f18cbe5
Saddam-tech <salokhiddinov0727@gmail.com>
2021-01-19 05:05:11 +0900

    Test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Resume from "./components/Resume";
-import Certificates from "./components/Certificates"; 
+import Certificates from "./components/Certificates";
 
 function App() {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  return !loading ? (
     <div className="App">
       <Header />
       <About />
       <Resume />
       <Certificates />
     </div>
+  ) : (
+    <p
+      style={{
+        textAlign: "center",
+        verticalAlign: "center",
+        lineHeight: "750px",
+        fontFamily: "Optima",
+        fontSize: "30px",
+      }}
+    >
+      Loading...
+    </p>
   );
 }
 
