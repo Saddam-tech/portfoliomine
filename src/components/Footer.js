cdf3bf457368250c24976ff23b101f66d10c07a1
Saddam-tech <salokhiddinov0727@gmail.com>
2022-02-23 13:32:50 +0900

    latest code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,76 +10,73 @@ import { Link as Scroll } from "react-scroll";
 
 const Footer = () => {
   const date = new Date().getFullYear();
-  console.log(date);
 
   return (
-    <React.Fragment>
-      <Container>
-        <div>
-          <p>Keep in touch with me on social media:</p>
-          <Fade bottom>
-            <div className="icons">
-              <a href="https://github.com/Saddam-tech" target="blank">
-                <GitHubIcon
-                  style={{
-                    width: "30px",
-                    height: "30px",
-                    color: "white",
-                    opacity: "0.5",
-                  }}
-                />
-              </a>
-              <a href="mailto:salokhiddinov0727@gmail.com" target="blank">
-                <EmailIcon
-                  style={{
-                    width: "30px",
-                    height: "30px",
-                    color: "white",
-                    opacity: "0.5",
-                  }}
-                />
-              </a>
-              <a
-                href="https://www.facebook.com/saddam.salokhiddinov"
-                target="blank"
-              >
-                {" "}
-                <FacebookIcon
-                  style={{
-                    width: "30px",
-                    height: "30px",
-                    color: "white",
-                    opacity: "0.5",
-                  }}
-                />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/saddam-salokhiddinov-35a9351a7/"
-                target="blank"
-              >
-                {" "}
-                <LinkedInIcon
-                  style={{
-                    width: "30px",
-                    height: "30px",
-                    color: "white",
-                    opacity: "0.5",
-                  }}
-                />
-              </a>
-            </div>
-          </Fade>
-          <div className="copyright">Copyright © {date} SaddamTech Inc.</div>
-          <div className="icon-container">
-            <Scroll to="header" smooth={true} duration={1000} spy={true}>
-              <ArrowDropDownCircleIcon
-                style={{ fontSize: "50px", color: "white", opacity: "0.2" }}
+    <Container>
+      <div>
+        <p>Keep in touch with me on social media:</p>
+        <Fade bottom>
+          <div className="icons">
+            <a href="https://github.com/Saddam-tech" target="blank">
+              <GitHubIcon
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  color: "white",
+                  opacity: "0.5",
+                }}
               />
-            </Scroll>
+            </a>
+            <a href="mailto:salokhiddinov0727@gmail.com" target="blank">
+              <EmailIcon
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  color: "white",
+                  opacity: "0.5",
+                }}
+              />
+            </a>
+            <a
+              href="https://www.facebook.com/saddam.salokhiddinov"
+              target="blank"
+            >
+              {" "}
+              <FacebookIcon
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  color: "white",
+                  opacity: "0.5",
+                }}
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/saddam-salokhiddinov-35a9351a7/"
+              target="blank"
+            >
+              {" "}
+              <LinkedInIcon
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  color: "white",
+                  opacity: "0.5",
+                }}
+              />
+            </a>
           </div>
+        </Fade>
+        <div className="copyright">Copyright © {date} SaddamTech Inc.</div>
+        <div className="icon-container">
+          <Scroll to="header" smooth={true} duration={1000} spy={true}>
+            <ArrowDropDownCircleIcon
+              style={{ fontSize: "50px", color: "white", opacity: "0.2" }}
+            />
+          </Scroll>
         </div>
-      </Container>
-    </React.Fragment>
+      </div>
+    </Container>
   );
 };
 
